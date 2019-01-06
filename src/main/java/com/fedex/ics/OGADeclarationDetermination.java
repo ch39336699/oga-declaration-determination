4ac5abc8e80c0b4b6399990de994405a24b145b3
3933666 <ch393366@gmail.com>
2019-01-05 22:23:31 -0700

    Initial Commit

---
+++ src/main/java/com/fedex/ics/OGADeclarationDetermination.java
@@ -0,0 +1,14 @@
+package com.fedex.ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OGADeclarationDetermination{
+
+	public static void main(String[] args) {
+		SpringApplication.run(OGADeclarationDetermination.class, args);
+	}
+
+}
+
